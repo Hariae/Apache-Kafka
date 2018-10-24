ca2e45c56558dde56a5c9cce61d5ebce89c24f85
Hariae <aehari2010@gmail.com>
2018-10-24 10:30:40 -0700

    DatabaseConnection

--- Kafka/Kafka-Intro/DatabaseConnection.js
+++ Kafka/Kafka-Intro/DatabaseConnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/HomeAway');
 
-var Userdetails = mongoose.model('Userdetails', {
+var Userdetails = mongoose.model('Userdetails',{
 
     'Username' : {
         type: String
